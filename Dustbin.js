7cb37bbdb345031b26e47e48dfe2b4dc3feb1bdb
SanidhyaMalviya <72500621+SanidhyaMalviya@users.noreply.github.com>
2020-11-07 16:29:34 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,18 +1,18 @@
 class Dustbin{
-    constructor(x,y,width,height){
-        this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
+    constructor(width,height){
+        //this.body = Bodies.rectangle(x,y,width,height,{isStatic:true});
         this.width = width;
         this.height = height;
         this.image = loadImage("dustbingreen.png");
         
-        World.add(world, this.body);
+        //World.add(world, this.body);
     }
     
     display(){
         var pos = this.body.position
         imageMode(CENTER)
         push();
-        image(this.image,pos.x,pos.y,this.width,this.height)
+        image(this.image,800,310,this.width,this.height)
         pop();
     }
 }
